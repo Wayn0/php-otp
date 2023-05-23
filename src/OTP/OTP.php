5bbc37c45d3fdce9ee731ca6f91a02504a7ce559
Wayne Oliver <wayne@instacom.co.za>
2023-05-23 15:31:38 +0200

    fix some typos and formatting

--- src/OTP/OTP.php
+++ src/OTP/OTP.php
@@ -10,73 +10,72 @@
 
 namespace OTP;
 
-class OTP {
+class OTP
+{
+    public $skey;  //BASE32 encoded secret key
+    public $algo;  //hashing algorithm
+    public $plen;  //One time pin length
 
-  public $skey;  //BASE32 encoded secret key
-  public $algo;  //hashing algorithm 
-  public $plen;  //One time pin length
-
-  /**
-   * Constructor for the OTP class
-   * @param string $key the secret key
-   * @param array $option, options array can contain the
-   * following keys :
-   *   @param integer plen : the length of the one time pin
-   *   @param string  algo : the algorithm used for the hmac hash function
-   *
-   * @return new OTP class.
-   */
-  public function __construct($secret, $options = Array()) {
-
-    $this->plen = isset($options['plen']) ? $option['plen'] : 6;
-    $this->algo = isset($options['algo']) ? $option['algo'] : 'sha1';
-    $this->skey = $secret;
-
-  }
-
-
-  /**
-   * Generate a one-time password
-   *
-   * @param integer $input : number used to seed the hmac hash function.
-   *
-   * @return integer the one-time password 
-   */
-  public function get_otp($input) {
-
-    $hash = hash_hmac($this->algo, $this->int_to_byte_string($input),$this->decode_key());
-    foreach(str_split($hash, 2) as $hex) { 
-      $hmac[] = hexdec($hex);
+    /**
+     * Constructor for the OTP class
+     * @param string $key the secret key
+     * @param array $option, options array can contain the
+     * following keys :
+     *   @param integer plen : the length of the one time pin
+     *   @param string  algo : the algorithm used for the hmac hash function
+     *
+     * @return new OTP class.
+     */
+    public function __construct($secret, $options = Array())
+    {
+        $this->plen = isset($options['plen']) ? $options['plen'] : 6;
+        $this->algo = isset($options['algo']) ? $options['algo'] : 'sha1';
+        $this->skey = $secret;
     }
-    $offset = $hmac[19] & 0xf;
-    $code = ($hmac[$offset+0] & 0x7F) << 24 |
-        ($hmac[$offset + 1] & 0xFF) << 16 |
-        ($hmac[$offset + 2] & 0xFF) << 8 |
-        ($hmac[$offset + 3] & 0xFF);
-    return $code % pow(10, $this->plen);
-  }
 
-  /**
-   * Returns the binary value of the base32 encoded secret skey
-   * 
-   * @return binary secret key
-   */
-  public function decode_key() {
-    return \Base32\Base32::decode($this->skey);
-  }
 
-  /**
-   * Turns an integer in a OATH bytestring
-   * @param integer $int
-   * 
-   * @return string bytestring
-   */
-  public function int_to_byte_string($int) {
-    $result = Array();
-    while($int != 0) {
-      $result[] = chr($int & 0xFF);
-      $int >>= 8;
+    /**
+     * Generate a one-time password
+     *
+     * @param integer $input : number used to seed the hmac hash function.
+     *
+     * @return integer the one-time password
+     */
+    public function get_otp($input)
+    {
+        $hash = hash_hmac($this->algo, $this->int_to_byte_string($input), $this->decode_key());
+        foreach(str_split($hash, 2) as $hex) {
+            $hmac[] = hexdec($hex);
+        }
+        $offset = $hmac[19] & 0xf;
+        $code = ($hmac[$offset+0] & 0x7F) << 24 |
+            ($hmac[$offset + 1] & 0xFF) << 16 |
+            ($hmac[$offset + 2] & 0xFF) << 8 |
+            ($hmac[$offset + 3] & 0xFF);
+        return $code % pow(10, $this->plen);
     }
-    return str_pad(join(array_reverse($result)), 8, "\000", STR_PAD_LEFT);
-  }
+
+    /**
+     * Returns the binary value of the base32 encoded secret skey
+     *
+     * @return binary secret key
+     */
+    public function decode_key() {
+        return \Base32\Base32::decode($this->skey);
+    }
+
+    /**
+     * Turns an integer in a OATH bytestring
+     * @param integer $int
+     *
+     * @return string bytestring
+     */
+    public function int_to_byte_string($int) {
+        $result = Array();
+        while($int != 0) {
+            $result[] = chr($int & 0xFF);
+            $int >>= 8;
+        }
+        return str_pad(join(array_reverse($result)), 8, "\000", STR_PAD_LEFT);
+    }
 }
--- src/OTP/TOTP.php
+++ src/OTP/TOTP.php
@@ -11,52 +11,57 @@
 namespace OTP;
 
 class TOTP extends OTP {
-  /**
-   * The interval in seconds for a one-time password timeframe
-   * Defaults to 30
-   * @var integer
-   */
-  public $interval;
+    /**
+     * The interval in seconds for a one-time password time frame
+     * Defaults to 30
+     * @var integer
+     */
+    public $interval;
 
-  public function __construct($s, $opt = Array()) {
-      $this->interval = isset($opt['interval']) ? $opt['interval'] : 30;
-    parent::__construct($s, $opt);
-  }
+    public function __construct($s, $opt = Array())
+    {
+        $this->interval = isset($opt['interval']) ? $opt['interval'] : 30;
+        parent::__construct($s, $opt);
+    }
 
-  /**
-   *  Get the password for a specific timestamp value
-   *
-   *  @param integer $timestamp the timestamp which is timecoded and
-   *  used to seed the hmac hash function.
-   *  @return integer the One Time Password
-   */
-  public function at($timestamp) {
-    return $this->get_otp($this->timecode($timestamp));
-  }
+    /**
+     *  Get the password for a specific timestamp value
+     *
+     *  @param integer $timestamp the timestamp which is timecoded and
+     *  used to seed the hmac hash function.
+     *  @return integer the One Time Password
+     */
+    public function at($timestamp)
+    {
+        return $this->get_otp($this->timecode($timestamp));
+    }
 
-  /**
-   *  Get the password for the current timestamp value
-   *
-   *  @return integer the current One Time Password
-   */
-  public function now() {
-    return $this->get_otp($this->timecode(time()));
-  }
+    /**
+     *  Get the password for the current timestamp value
+     *
+     *  @return integer the current One Time Password
+     */
+    public function now()
+    {
+        return $this->get_otp($this->timecode(time()));
+    }
 
-  /**
-   * Verify if a password is valid for a specific counter value
-   *
-   * @param integer $otp the one-time password
-   * @param integer $timestamp the timestamp for the a given time, defaults to current time.
-   * @return  bool true if the counter is valid, false otherwise
-   */
-  public function verify($otp, $timestamp = null) {
-    if($timestamp === null)
-      $timestamp = time();
-    return ($otp == $this->at($timestamp));
-  }
+    /**
+     * Verify if a password is valid for a specific counter value
+     *
+     * @param integer $otp the one-time password
+     * @param integer $timestamp the timestamp for the a given time, defaults to current time.
+     * @return  bool true if the counter is valid, false otherwise
+     */
+    public function verify($otp, $timestamp = null)
+    {
+        if($timestamp === null) {
+            $timestamp = time();
+        }
+        return ($otp == $this->at($timestamp));
+    }
 
-  /**
+    /**
      * Returns the uri for a specific secret for totp method.
      * Can be encoded as a image for simple configuration in
      * Google Authenticator.
@@ -64,18 +69,19 @@ class TOTP extends OTP {
      * @param string $name the name of the account / profile
      * @return string the uri for the hmac secret
      */
-    public function provisioning_uri($name) {
-      return "otpauth://totp/".urlencode($name)."?secret={$this->skey}";
+    public function provisioning_uri($name)
+    {
+        return "otpauth://totp/".urlencode($name)."?secret={$this->skey}";
     }
 
-  /**
-   * Transform a timestamp in a counter based on specified internal
-   *
-   * @param integer $timestamp
-   * @return integer the timecode
-   */
-  protected function timecode($timestamp) {
-    return (int)( (((int)$timestamp * 1000) / ($this->interval * 1000)));
-  }
+    /**
+     * Transform a timestamp in a counter based on specified internal
+     *
+     * @param integer $timestamp
+     * @return integer the timecode
+     */
+    protected function timecode($timestamp)
+    {
+        return (int)( (((int)$timestamp * 1000) / ($this->interval * 1000)));
+    }
 }
-
